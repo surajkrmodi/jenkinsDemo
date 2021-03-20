fdb458c8bdd88bb21434b6f3e7d1f86ae5cc43e4
surajkrmodi <modisuraj61@gmail.com>
2021-03-20 12:41:57 +0530

    Second commit

--- src/main/java/p1/demo1.java
+++ src/main/java/p1/demo1.java
@@ -3,5 +3,6 @@ package p1;
 public class demo1 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to jenkins demo");
+		System.out.println("Welcome to jenkins demo");
 	}
 }
